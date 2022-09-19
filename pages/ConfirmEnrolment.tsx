039001e5517e7aa2aa41d6c8ad443b9666073a68
Ronald MacDonald <ronald@rmacd.com>
2022-09-19 21:31:33 +0100

    add fonts to plist ; start handling enrolment confirmation

--- pages/ConfirmEnrolment.tsx
+++ pages/ConfirmEnrolment.tsx
@@ -1,7 +1,6 @@
 import React, {useEffect, useState} from 'react';
-import {List, Paragraph, Title} from 'react-native-paper';
-import {styles} from '../utils/LocalStyles';
-import {View} from 'react-native';
+import {List, Title, Text, Paragraph, Colors, Checkbox, Button} from 'react-native-paper';
+import {StyleSheet, View} from 'react-native';
 import {DefaultViewWrapper} from '../utils/DefaultViewWrapper';
 import {PermissionDTO, PreEnrolmentResponse, ProjectPermissions} from '../generated/UsherTypes';
 import {RouteProp} from '@react-navigation/native';
@@ -9,20 +8,36 @@ import {BASE_API_URL} from '@env';
 import {LoadingSpinner} from '../components/LoadingSpinner';
 import { useWindowDimensions } from 'react-native';
 import RenderHtml from 'react-native-render-html';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+const styles = StyleSheet.create({
+    header: {
+        borderTopRightRadius: 5,
+        borderTopLeftRadius: 5,
+        paddingHorizontal: 10,
+        backgroundColor: Colors.grey400
+    },
+    container: {
+        backgroundColor: "#ddd",
+        borderBottomRightRadius: 5,
+        borderBottomLeftRadius: 5,
+        paddingHorizontal: 10,
+        marginBottom: 15,
+    },
+});
 
 function RenderAsHtml(props: { width: number, contents: string | undefined, title: string }) {
     if (!props.contents) {
         return <></>;
     }
+
     return (
         <>
-            <Title>{props.title}</Title>
-            <RenderHtml baseStyle={{
-                backgroundColor: "#ddd",
-                borderRadius: 10,
-                paddingHorizontal: 10,
-                marginBottom: 15,
-            }} contentWidth={props.width} source={{html: props.contents || ""}} />
+            <View style={styles.header}>
+                <Title style={{}}>{props.title}</Title>
+            </View>
+
+            <RenderHtml baseStyle={styles.container} contentWidth={props.width} source={{html: props.contents || ""}} />
         </>
     );
 }
@@ -33,8 +48,11 @@ function RenderPermissions(props: { allPermissions: PermissionDTO[] | undefined,
     }
     return (
         <>
+            <View style={styles.header}>
             <Title>Project Permissions</Title>
+            </View>
 
+            <View style={styles.container}>
             {Boolean(props.appPermissions.length === 0) && (
                 <List.Item
                     title="No permissions listed"
@@ -47,17 +65,26 @@ function RenderPermissions(props: { allPermissions: PermissionDTO[] | undefined,
                     description={props.allPermissions?.find(m => m.name === p)?.description}/>;
             })}
             </>
+            </View>
         </>
-    )
+    );
 }
 
 export const ConfirmEnrolment = ({route}: {route: RouteProp<any>}) => {
 
     const preEnrolment = route.params?.project as PreEnrolmentResponse;
 
+    Icon.loadFont();
+
     const [loading, setLoading] = useState(true);
     const [permissions, setPermissions] = useState([] as PermissionDTO[]);
     const { width } = useWindowDimensions();
+    const [checkedTerms, setCheckedTerms] = useState(false);
+    const [checkedData, setCheckedData] = useState(false);
+
+    function completeEnrolment() {
+
+    }
 
     useEffect(() => {
         fetch(BASE_API_URL + "/permissions", {})
@@ -84,12 +111,32 @@ export const ConfirmEnrolment = ({route}: {route: RouteProp<any>}) => {
     return (
         <DefaultViewWrapper>
             <View>
-                <Title>
-                    Project: {preEnrolment?.projectName}
-                </Title>
-                <RenderAsHtml width={width} contents={preEnrolment.projectDescription} title={"Description"}/>
+                <View style={{borderRadius: 20, backgroundColor: Colors.blue500, marginVertical: 20}}>
+                <Paragraph style={{padding: 10, color: Colors.white}}>
+                    The following description and project terms have been provided by the study lead to help you
+                    understand what data is being collected and how this might affect you. Please read all terms
+                    below before confirming participation in the project.
+                </Paragraph>
+                </View>
+                <RenderAsHtml width={width} contents={preEnrolment.projectDescription} title={"Project Description"}/>
                 <RenderAsHtml width={width} contents={preEnrolment.projectTerms} title={"Terms and Conditions"}/>
                 <RenderPermissions allPermissions={permissions} appPermissions={preEnrolment.requiredPermissions}/>
+                <Title>Confirm enrolment</Title>
+                <Text>Please check each of the following boxes before completing enrolment:</Text>
+                <View>
+                    <Checkbox.Item label="I agree to the project terms and conditions"
+                                   onPress={() => setCheckedTerms(!checkedTerms)}
+                                   status={(checkedTerms) ? "checked" : "unchecked"} />
+                    <Checkbox.Item label="I understand the types of data that this project may gather about me"
+                                   onPress={() => setCheckedData(!checkedData)}
+                                   status={(checkedData) ? "checked" : "unchecked"} />
+                    <Button
+                        onPress={() => completeEnrolment()}
+                        disabled={(!checkedTerms || !checkedData)}>
+                        Complete enrolment
+                    </Button>
+                </View>
+                <View style={{marginBottom: 50}}></View>
             </View>
         </DefaultViewWrapper>
     );
